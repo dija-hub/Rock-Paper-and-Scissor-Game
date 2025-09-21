340d5b781af43188bdd66fa27398fbb6b53a28c4
dija <dija5408@gmail.com>
2025-09-21 19:14:39 +0500

    game

--- game.js
+++ game.js
@@ -1,5 +1,10 @@
 let userScore=0;
 let compScore=0;
 
-const choice= document.querySelectorAll(".choice");
+const choices= document.querySelectorAll(".choice");
+const genCompChoice=() =>{
+    let options=["rock","paper","scissor"];
+   const randIdx =Math.floor( Math.random() * 3);
+   return options [randIdx];
+}
 
